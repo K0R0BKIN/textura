80d92846bddcb2bab767d8a6c714fe94f43959e6
Nikita Korobkin <nikita.korobkin.personal@gmail.com>
2025-11-25 15:18:15 +0300

    Style SearchBox

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+function SearchBox() {
+  return (
+    <div className="flex">
+      <input
+        type="text"
+        placeholder="Look up definitions..."
+        className="shadow-claude w-xs rounded-2xl border border-gray-200 px-4 py-4 text-base text-gray-700 transition-colors hover:border-gray-300 sm:w-lg"
+      />
+    </div>
+  );
+}
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="flex min-h-screen items-center justify-center">
+        <SearchBox />
+      </div>
+    </>
+  );
 }
 
 export default App;
