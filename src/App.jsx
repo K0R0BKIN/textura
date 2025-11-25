d745e1b2aea85f8f69b58cb63e67252177376e9f
Nikita Korobkin <nikita.korobkin.personal@gmail.com>
2025-11-25 17:57:30 +0300

    Add search button in SearchBox

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
+import { useState } from "react";
+import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
+
 function SearchBox() {
+  const [isFocused, setIsFocused] = useState(false);
+
   return (
     <div className="flex">
-      <input
-        type="text"
-        placeholder="Look up definitions..."
-        className="shadow-claude w-xs rounded-2xl border border-gray-200 px-4 py-4 text-base text-gray-700 transition-colors hover:border-gray-300 sm:w-lg"
-      />
+      <div
+        className={`shadow-claude group flex w-xs gap-2 rounded-2xl border bg-white p-2 pl-4 transition-colors sm:w-lg ${
+          isFocused
+            ? "border-gray-300"
+            : "border-gray-200 hover:border-gray-300"
+        }`}
+      >
+        <input
+          type="text"
+          placeholder="Look up definitions..."
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+          className="w-full text-base text-gray-700 outline-none"
+          autoFocus
+        />
+        <button
+          className="bg-brown-9 hover:bg-brown-10 flex h-8 w-8 items-center justify-center rounded-lg text-white disabled:pointer-events-none disabled:opacity-50"
+          disabled
+        >
+          <MagnifyingGlassIcon className="h-5 w-5" />
+        </button>
+      </div>
     </div>
   );
 }
@@ -13,7 +35,7 @@ function SearchBox() {
 function App() {
   return (
     <>
-      <div className="flex min-h-screen items-center justify-center">
+      <div className="bg-brown-2 flex min-h-screen items-center justify-center">
         <SearchBox />
       </div>
     </>
