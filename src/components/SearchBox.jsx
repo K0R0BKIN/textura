4f82e32018fb0dfee84f24f4875bcb6c0d89c18c
Nikita Korobkin <nikita.korobkin.personal@gmail.com>
2025-12-09 14:29:16 +0300

    Refactor SearchBar input styles

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -37,7 +37,7 @@ function SearchBar({
         onBlur={onBlur}
         onFocus={onFocus}
         onKeyDown={onKeyDown}
-        className="placeholder:text-(--text-11) w-full text-base leading-none font-light text-black outline-none"
+        className="w-full text-ui text-black outline-none placeholder:text-(--text-11)"
         autoFocus
       />
       <button
