2c885b911a6358e81cbd9e4d0acfbc572aa10ef7
Nikita Korobkin <nikita.korobkin.personal@gmail.com>
2025-12-12 01:16:09 +0300

    Refactor ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,35 +1,30 @@
+// eslint.config.js
 import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
 import globals from "globals";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import eslintConfigPrettier from "eslint-config-prettier/flat";
-import { defineConfig, globalIgnores } from "eslint/config";
+import tseslint from "typescript-eslint";
 
 export default defineConfig([
-  globalIgnores(["dist"]),
+  { ignores: ["dist", "build", "node_modules", "coverage"] },
+
+  js.configs.recommended,
+  tseslint.configs.recommended,
+
+  react.configs.flat.recommended,
+  react.configs.flat["jsx-runtime"],
+  reactHooks.configs.flat.recommended,
+
   {
-    files: ["**/*.{js,jsx}"],
-    extends: [
-      js.configs.recommended,
-      react.configs.flat.recommended,
-      react.configs.flat["jsx-runtime"],
-      reactHooks.configs["recommended-latest"],
-      reactRefresh.configs.vite,
-    ],
+    files: ["**/*.{ts,tsx,js,jsx}"],
     languageOptions: {
-      ecmaVersion: 2020,
       globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: "latest",
-        ecmaFeatures: { jsx: true },
-        sourceType: "module",
+    },
+    settings: {
+      react: {
+        version: "detect",
       },
     },
-    rules: {
-      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
-      "react/prop-types": "off",
-    },
   },
-  eslintConfigPrettier,
 ]);
