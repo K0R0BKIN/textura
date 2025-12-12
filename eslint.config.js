2630ad36c9c852b341cb6c52c9b3df486446b7fd
Nikita Korobkin <nikita.korobkin.personal@gmail.com>
2025-12-12 13:48:36 +0300

    Use `eslint.config.js` from `react-ts`

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,23 @@
-// eslint.config.js
 import js from "@eslint/js";
-import { defineConfig } from "eslint/config";
 import globals from "globals";
-import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  { ignores: ["dist", "build", "node_modules", "coverage"] },
-
-  js.configs.recommended,
-  tseslint.configs.recommended,
-
-  react.configs.flat.recommended,
-  react.configs.flat["jsx-runtime"],
-  reactHooks.configs.flat.recommended,
-
+  globalIgnores(["dist"]),
   {
-    files: ["**/*.{ts,tsx,js,jsx}"],
+    files: ["**/*.{ts,tsx}"],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
     languageOptions: {
+      ecmaVersion: 2020,
       globals: globals.browser,
-    },
-    settings: {
-      react: {
-        version: "detect",
-      },
     },
   },
 ]);
