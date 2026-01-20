d3b87d2914ec873319d2da750e5fa1ece738bc20
Nikita Korobkin <nikita.korobkin.personal@gmail.com>
2026-01-21 00:11:41 +0300

    Style Composer

--- components/composer.tsx
+++ components/composer.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { Search } from 'lucide-react';
+import { cn } from '@/lib/utils';
 import {
   InputGroup,
   InputGroupAddon,
@@ -9,11 +11,43 @@ import {
 
 export function Composer() {
   return (
-    <InputGroup>
-        <InputGroupInput placeholder="Type to search..." />
-        <InputGroupAddon align="inline-end">
-          <InputGroupButton variant="secondary">Search</InputGroupButton>
-        </InputGroupAddon>
-      </InputGroup>
+    <InputGroup
+      className={cn(
+        'h-[56px] rounded-[20px] border-(--sand-6) bg-(--sand-1) shadow-[0_4px_20px_rgba(32,32,32,0.06)] transition-colors',
+
+        // Hover state
+        'hover:border-(--sand-8)',
+        'hover:shadow-[0_4px_20px_rgba(32,32,32,0.08)]',
+
+        // Focus state
+        'has-[[data-slot=input-group-control]:focus-visible]:border-(--sand-8)',
+        'has-[[data-slot=input-group-control]:focus-visible]:ring-0',
+        'has-[[data-slot=input-group-control]:focus-visible]:shadow-[0_4px_20px_rgba(32,32,32,0.08)]',
+
+        // Dark mode
+        'dark:bg-(--sand-3)',
+
+        // Dark mode interactions
+        'dark:hover:border-(--sand-7)',
+        'dark:has-[[data-slot=input-group-control]:focus-visible]:border-(--sand-7)',
+      )}
+    >
+      <InputGroupInput
+        className="px-[16px]"
+        placeholder="Look up definitions..."
+      />
+      <InputGroupAddon
+        className="pr-[8px] has-[>button]:mr-0"
+        align="inline-end"
+      >
+        <InputGroupButton
+          variant="default"
+          size="icon-lg"
+          className="dark:bg-(--brown-8) dark:hover:bg-(--brown-9)"
+        >
+          <Search />
+        </InputGroupButton>
+      </InputGroupAddon>
+    </InputGroup>
   );
 }
