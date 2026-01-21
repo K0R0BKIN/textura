53846fd7d65b84a2708c6035bcdbe2777b97b1ca
Nikita Korobkin <nikita.korobkin.personal@gmail.com>
2026-01-21 13:57:01 +0300

    Adjust logo color in dark mode

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,7 +1,7 @@
 // components/logo.tsx
 export function Logo() {
   return (
-    <h1 className="text-foreground text-center font-serif text-7xl font-black tracking-tight select-none">
+    <h1 className="text-foreground text-center font-serif text-7xl font-black tracking-tight select-none dark:text-(--sand-12)/80">
       Textura
     </h1>
   );
