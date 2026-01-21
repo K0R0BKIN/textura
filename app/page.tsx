17040ff6402eacc2d9ec717ffcad274387ceae83
Nikita Korobkin <nikita.korobkin.personal@gmail.com>
2026-01-21 08:44:52 +0300

    Add Logo to Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Logo } from '@/components/logo';
 import { Composer } from '@/components/composer';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <div className="w-full max-w-lg">
+    <div className="flex min-h-screen items-start pt-72 justify-center">
+      <div className="flex w-full max-w-lg flex-col gap-4">
+        <Logo></Logo>
         <Composer />
       </div>
     </div>
---
+++ components/logo.tsx
@@ -0,0 +1,8 @@
+// components/logo.tsx
+export function Logo() {
+  return (
+    <h1 className="text-foreground text-center text-7xl font-black tracking-tight select-none">
+      Textura
+    </h1>
+  );
+}
