d4d66e7baaa4a922442245b819c046bbbf22976f
Nikita Korobkin <nikita.korobkin.personal@gmail.com>
2026-01-21 09:07:39 +0300

    Logo uses serif font

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,7 +1,7 @@
 // components/logo.tsx
 export function Logo() {
   return (
-    <h1 className="text-foreground text-center text-7xl font-black tracking-tight select-none">
+    <h1 className="text-foreground text-center font-serif text-7xl font-black tracking-tight select-none">
       Textura
     </h1>
   );
