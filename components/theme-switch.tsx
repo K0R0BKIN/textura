4359747b698a2f38b2c40afbdfab8783cab1f276
Nikita Korobkin <nikita.korobkin.personal@gmail.com>
2026-01-23 21:43:48 +0300

    Add ThemeSwitch
    Displays resolvedTheme as an icon
    
    - Add Sand Alpha

---
+++ components/theme-switch.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { useTheme } from 'next-themes';
+import { Sun, Moon } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+
+export default function ThemeSwitch() {
+  const { resolvedTheme } = useTheme();
+
+  return (
+    <Button
+      variant="ghost"
+      size="icon-lg"
+      className="text-foreground dark:text-muted-foreground rounded-full hover:bg-(--sand-a3)/50 dark:hover:bg-(--sand-a3)"
+    >
+      {resolvedTheme === 'dark' ? <Moon /> : <Sun />}
+    </Button>
+  );
+}
