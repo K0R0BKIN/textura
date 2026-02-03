55f4bd482975b9afe5ed7370b66ba9e0c536e0d0
Nikita Korobkin <nikita.korobkin.personal@gmail.com>
2026-02-03 21:04:24 +0300

    Add Logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import { Logo } from '@/components/logo';
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <Logo></Logo>
+    </div>
+  );
 }
---
+++ components/logo.tsx
@@ -0,0 +1,7 @@
+export function Logo() {
+  return (
+    <h1 className="text-foreground text-7xl leading-none font-black tracking-tight select-none">
+      Textura
+    </h1>
+  );
+}
