aae2fb960ee5557cb4799331447826afb8b97268
Nikita Korobkin <nikita.korobkin.personal@gmail.com>
2026-02-05 16:27:55 +0300

    Prune SearchBox Input attrs

--- components/search-box.tsx
+++ components/search-box.tsx
@@ -4,9 +4,6 @@ export function SearchBox() {
   return (
     <InputGroup variant="card" size="lg" className="w-lg">
       <InputGroupInput
-        type="search"
-        name="query"
-        enterKeyHint="search"
         placeholder="Look up definitions…"
         aria-label="Search query"
         autoFocus
