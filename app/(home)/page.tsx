7bd0118e31f13993c8373b3bef0acb22d119516f
Nikita Korobkin <nikita.korobkin.personal@gmail.com>
2026-04-03 14:23:39 +0300

    Key home searchbox by pathname

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,20 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
 import { Logo } from '@/components/logo';
 import { SearchBox } from '@/components/search-box';
+
+function HomeSearchBox() {
+  const pathname = usePathname();
+
+  return <SearchBox key={pathname} />;
+}
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center gap-3">
       <Logo />
-      <SearchBox />
+      <HomeSearchBox />
     </div>
   );
 }
